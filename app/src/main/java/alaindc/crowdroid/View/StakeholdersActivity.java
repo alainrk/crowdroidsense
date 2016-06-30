a76bdfe76169610aa98773ac14cde3893d8071e9
alainrk <alain.narko@gmail.com>
2016-06-30 21:35:34 +0200

    All client cycle for subscription DONE

--- app/src/main/java/alaindc/crowdroid/View/StakeholdersActivity.java
+++ app/src/main/java/alaindc/crowdroid/View/StakeholdersActivity.java
@@ -13,6 +13,8 @@ import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -27,6 +29,7 @@ import org.json.JSONObject;
 import alaindc.crowdroid.Constants;
 import alaindc.crowdroid.PositionIntentService;
 import alaindc.crowdroid.R;
+import alaindc.crowdroid.RadioUtils;
 import alaindc.crowdroid.SendIntentService;
 import alaindc.crowdroid.SendRequestTask;
 import alaindc.crowdroid.SensorsIntentService;
@@ -37,6 +40,8 @@ import de.uzl.itm.ncoap.application.client.CoapClient;
 public class StakeholdersActivity extends AppCompatActivity {
 
     LinearLayout linearLayout;
+    Button updateStakeButton;
+    Button sendStakeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,52 @@ public class StakeholdersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stakeholders);
 
         linearLayout = (LinearLayout) findViewById(R.id.subscribesLinearLayout);
+        updateStakeButton = (Button) findViewById(R.id.updateStakeButton);
+        sendStakeButton = (Button) findViewById(R.id.sendStakeButton);
+
+        updateStakeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent subscrIntent = new Intent(getApplicationContext(), SendIntentService.class);
+                subscrIntent.setAction(Constants.ACTION_GETSUBSCRIPTION);
+                getApplicationContext().startService(subscrIntent);
+            }
+        });
+
+        sendStakeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                try {
+                    JSONArray jsonArr = new JSONArray();
+
+                    JSONObject jsonObj = new JSONObject();
+                    jsonObj.put("user", RadioUtils.getMyDeviceId(getApplicationContext()));
+                    jsonArr.put(jsonObj);
+
+                    for (int i = 0; i < linearLayout.getChildCount(); i++) {
+                        View view = linearLayout.getChildAt(i);
+                        if (view instanceof CheckBox) {
+                            CheckBox c = (CheckBox) view;
+                            if (c.isChecked()) {
+                                jsonObj = new JSONObject();
+                                jsonObj.put("id", c.getId());
+                                jsonArr.put(jsonObj);
+                            }
+                        }
+                    }
+
+                    String body = jsonArr.toString();
+
+                    Intent subscrIntent = new Intent(getApplicationContext(), SendIntentService.class);
+                    subscrIntent.setAction(Constants.ACTION_UPDATESUBSCRIPTION);
+                    subscrIntent.putExtra(Constants.EXTRA_BODY_UPDATESUBSCRIPTION, body);
+                    getApplicationContext().startService(subscrIntent);
+
+                } catch (JSONException e) {
+                    return;
+                }
+            }
+        });
 
         Intent subscrIntent = new Intent(getApplicationContext(), SendIntentService.class);
         subscrIntent.setAction(Constants.ACTION_GETSUBSCRIPTION);
@@ -55,7 +106,9 @@ public class StakeholdersActivity extends AppCompatActivity {
                 if (intent.getAction().equals(Constants.INTENTVIEW_RECEIVED_SUBSCRIPTION)) {
                     String response = intent.getStringExtra(Constants.EXTRAVIEW_RECEIVED_SUBSCRIPTION);
 
-                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+                    linearLayout.removeAllViews();
 
                     try {
                         JSONArray jsonArray = new JSONArray(response);
